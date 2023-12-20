b40fc75efcce332bafd4b971a11e73922a568676
Sauter <gabriel.andrade.sauter@gmail.com>
2023-12-20 13:09:47 -0300

    Added initial check in todo

--- app/src/main/java/com/example/todo/entities/Todo.kt
+++ app/src/main/java/com/example/todo/entities/Todo.kt
@@ -4,6 +4,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
-data class Todo(val title: String){
-    var checked by mutableStateOf(false)
+data class Todo(val title: String, val initialCheck: Boolean = false){
+    var checked by mutableStateOf(initialCheck)
 }
